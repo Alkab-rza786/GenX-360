858ecec882d1234cf1b6490cf317b8e67f3019f3
anishkumar1412 <anishkumarcbsa@gmail.com>
2025-04-16 17:18:58 +0530

    changes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Navbar from './components/Navbar';
-import Home from './components/Home';
-import AuthForm from './components/AuthForm';
-import MedicalRecordForm from './Components/MedicalRecordForm';
+import Navbar from './components/Navbar.jsx';
+import Home from './components/Home.jsx';
+import AuthForm from './components/AuthForm.jsx';
+import MedicalRecordForm from './Components/MedicalRecordForm.jsx';
 
-import Profile from './Components/Profile';
-import EmergencyInfo from './Components/EmergencyInfo';
-import DiseaseHistory from './Components/DeseaseHistory';
+import Profile from './Components/Profile.jsx';
+import EmergencyInfo from './Components/EmergencyInfo.jsx';
+import DiseaseHistory from './Components/DeseaseHistory.jsx';
 
 function App() {
   return (
